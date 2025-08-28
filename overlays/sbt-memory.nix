31b8ce2c66654c19ea587e4e4ec3b41ce168d9fe
Martin Pallmann <132760+martinpallmann@users.noreply.github.com>
2025-08-28 16:38:07 +0200

    Add sbt overlay

--- overlays/sbt-memory.nix
+++ overlays/sbt-memory.nix
@@ -1,8 +1,15 @@
 final: prev: {
   sbt = prev.sbt.overrideAttrs (old: {
+    nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ prev.makeWrapper ];
     postInstall = (old.postInstall or "") + ''
-      wrapProgram $out/bin/sbt \
-        --add-flags "-J-Xms512m -J-Xmx2g"
+      if [ -x "$out/bin/sbt" ]; then
+        wrapProgram "$out/bin/sbt" \
+          --add-flags "-J-Xms512m -J-Xmx2g"
+      fi
+      if [ -x "$out/bin/sbtn" ]; then
+        wrapProgram "$out/bin/sbtn" \
+          --add-flags "-J-Xms512m -J-Xmx2g"
+      fi
     '';
   });
 }
