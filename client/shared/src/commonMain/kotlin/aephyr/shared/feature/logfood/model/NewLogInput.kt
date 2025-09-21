a7d71868c76eab76582be8764d73ec1ee874d47e
Martin Pallmann <132760+martinpallmann@users.noreply.github.com>
2025-09-21 13:45:24 +0200

    refactor: broken

--- client/shared/src/commonMain/kotlin/aephyr/shared/feature/logfood/model/NewLogInput.kt
+++
@@ -1,9 +0,0 @@
-package aephyr.shared.feature.logfood.model
-
-data class NewLogInput(
-    val consumedAt: DietMoment,
-    val name: FoodName,
-    val portion: Quantity?,
-    val energy: KiloJoule?,
-    val macros: Macros?
-)
